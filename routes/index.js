84c4b6dc0eb7a6a6dcad64fd5585503c4f130a6c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-02 14:43:15 -0700

    proxy headers

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,8 @@ const apicache = require('apicache');
 
 const API_BASE_URL = "https://api.github.com";
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
-const headers = {
+const enforcedHeaders = {
   "User-Agent": "ZenithProxy/1.1",
-  "Accept": "application/vnd.github+json",
-  "X-GitHub-Api-Version": "2022-11-28",
   "Connection": "close",
   "Authorization": `Bearer ${API_KEY_VALUE}`
 }
@@ -25,15 +23,19 @@ router.get('/**', cache('2 minutes'), async (req, res, next) => {
       next(new Error("Unsupported route: " + req.url))
       return;
     }
+    let reqHeaders = req.headers;
+    reqHeaders['User-Agent'] = enforcedHeaders['User-Agent'];
+    reqHeaders['Connection'] = enforcedHeaders['Connection'];
+    reqHeaders['Authorization'] = enforcedHeaders['Authorization'];
     const options = {
-      headers: headers
+      headers: reqHeaders
     }
     const dest = `${API_BASE_URL}${req.url}`
     console.log(dest)
     const apiRes = await needle('get', dest, options);
     const data = apiRes.body;
     console.log(`${apiRes.statusCode} ${dest}`)
-    res.status(apiRes.statusCode).json(data);
+    res.status(apiRes.statusCode).set(apiRes.headers).send(data);
   } catch (error) {
     error.
     next(error);
